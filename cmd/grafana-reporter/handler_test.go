dcbddd8e56ad53b05917b0511b43346ea23d71b0
emanzx <emanzx@gmail.com>
2021-12-03 17:24:41 +0800

    update reference

--- cmd/grafana-reporter/handler_test.go
+++ cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/emanzx/reporter/grafana"
+	"github.com/emanzx/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/emanzx/reporter/grafana"
+	"github.com/emanzx/reporter/report"
 	"github.com/gorilla/mux"
 )
 
