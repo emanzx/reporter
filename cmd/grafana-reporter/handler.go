7dca0234e9e69b0c4a1497f9a7ef31c8ef6bef5b
emanzx <emanzx@gmail.com>
2021-12-03 17:17:59 +0800

    fix url

--- cmd/grafana-reporter/handler.go
+++ cmd/grafana-reporter/handler.go
@@ -27,8 +27,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/emanzx/reporter/grafana"
+	"github.com/emanzx/reporter/report"
 	"github.com/gorilla/mux"
 )
 
